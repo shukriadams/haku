73bb49d4fb2b72ea9b915695be979bb64527bcca
Shukri Adams <shukri.adams@gmail.com>
2014-02-18 23:03:55 +0100

    Added gruntfile.js for startup.

---
+++ gruntfile.js
@@ -0,0 +1,36 @@
+module.exports = function(grunt) {
+
+    'use strict';
+
+  var sourceFolder = "bower_components/Haku";
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+      bower: {
+        install: { 
+          options : {
+            copy : false
+          }
+        }
+      },
+
+      copy: {
+          main: {
+              files: [
+                  { expand: true, cwd : sourceFolder, src: ['**', '!**/src/index.html', '!**/src/css-sass/*' ] }
+              ]
+          }
+      }
+     
+    
+  });
+
+  grunt.loadNpmTasks('grunt-bower-task');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+
+  // Default task(s).
+  grunt.registerTask('default', ['bower', 'copy']);
+
+};
