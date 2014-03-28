b7f215bcfcc9c2ec8704934a3d5ab91172661cf1
Shukri Adams <shukri.adams@gmail.com>
2014-03-28 14:04:52 +0100

    Added padding to ios 7 settings, fixing two issues (content floating above bar, link clicking not working).

--- src/app/settings-ios.js
+++ src/app/settings-ios.js
@@ -7,6 +7,12 @@
     $.ajaxSetup({ cache: true }); // fixes dreaded chromium-6 error in Android
     klon.logging = true;
 
+    // ios fix adds padding to top of app, making room for notification bar.
+    // Note : padding also fixes the error where links are not clickable
+    if (window.device && parseFloat(window.device.version) >= 7.0){
+        document.body.style.marginTop = "20px";
+    }
+
 
     // Override settings
     _.extend(haku.settings, {
