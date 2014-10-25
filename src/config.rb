e04db20fbdf73ba321fe8f4317c337e50e62ecb2
Shukri Adams <shukri.adams@gmail.com>
2014-10-25 13:13:34 +0200

    Reintroduced compass config file.

---
+++ src/config.rb
@@ -0,0 +1,10 @@
+# This is Haku's Compass config. It will be overwritten by Haku's own update scripts, so any changes
+# you add here may be lost.
+# To use this config, run "start compass watch" from the console in this directory (you need Compass
+# installed - http://compass-style.org)
+
+add_import_path "../bower_components/foundation/scss"
+add_import_path "../bower_components/foundation/scss/foundation"
+
+css_dir = "css"
+sass_dir = "css-sass"
